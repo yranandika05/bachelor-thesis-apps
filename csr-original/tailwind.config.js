471c29e8407bd4659acc90f96231df269349649e
yrkr80 <yohanes.ranandika.kirana@mni.thm.de>
2025-06-16 17:36:04 +0200

    csr-original: refactor posts page and adjust tailwind depedencies

---
+++ csr-original/tailwind.config.js
@@ -0,0 +1,12 @@
+import aspectRatio from '@tailwindcss/aspect-ratio'
+
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [aspectRatio],
+}
